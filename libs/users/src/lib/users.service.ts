a582b3ca0abf7d55f18cafa3f4c9c05b3a6032e2
Pedro Lemos <pedro9bee@gmail.com>
2022-11-22 08:11:53 +0000

    feat: hidden password

--- libs/users/src/lib/users.service.ts
+++ libs/users/src/lib/users.service.ts
@@ -3,12 +3,25 @@ import { PrismaClient, User } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+
 @Injectable()
 export class UsersService {
     public getUsers(): Promise<User[]> {
         return prisma.user.findMany({ take: 10 });
     }
-    public async register(@Body() body): Promise<User> {
-        return prisma.user.create({data: body});
+    public async register(@Body() body): Promise<Omit<User, "password">>{
+        const user = await prisma.user.create({data: body})
+        const userWithoutPassword = exclude(user, ['password'])
+        return userWithoutPassword;
     }
 }
+function exclude<User, Key extends keyof User>(
+    user: User,
+    keys: Key[]
+  ): Omit<User, Key> {
+    // eslint-disable-next-line prefer-const
+    for (let key of keys) {
+      delete user[key]
+    }
+    return user
+  }
